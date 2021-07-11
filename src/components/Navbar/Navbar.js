9d7bbde2c8492f4f45d00c36351aaf9de8473a81
JonahMac <87243089+JonahMac@users.noreply.github.com>
2021-07-10 22:52:04 -0300

    Mudança no titulo do NAV

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./navBar.css";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <span>React Kanban</span>
+      <span>React Kanban por Jhonata Nascimento(Curso)</span>
     </nav>
   );
 }
